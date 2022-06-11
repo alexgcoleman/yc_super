a99d388a8998f5a58b02522b188a7bf7fb6eee8a
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2022-06-12 01:34:00 +1000

    add additional getters to superdata

--- yc_super/superdata.py
+++ yc_super/superdata.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
+from yc_super.yearquarter import YearQuarter
 import pandas as pd
+
+WINDOW_TO_PAY = pd.Timedelta(days=28)
 
 
 class SuperData:
@@ -8,6 +11,8 @@ class SuperData:
     def __init__(self, payments: pd.DataFrame, disburs: pd.DataFrame):
         self.payments = payments
         self.disburs = disburs
+
+        self.employees = self._get_employees()
 
     def __str__(self):
         return f"SuperData({len(self.payments)} payments; {len(self.disburs)} disbursments)"
@@ -18,6 +23,22 @@ class SuperData:
     def get_emp_super_data(self, employee_code) -> EmpSuperData:
         return EmpSuperData(self, employee_code)
 
+    def get_payments_for_quarter(self, quarter: YearQuarter) -> pd.DataFrame:
+        return self.payments.loc[self.payments['quarter'] == quarter].copy()
+
+    def get_disbursments_for_quarter(self, quarter: YearQuarter) -> pd.DataFrame:
+        min_disburs_date = quarter.start + WINDOW_TO_PAY
+        max_disburs_date = quarter.end + WINDOW_TO_PAY
+
+        return self.disburs[
+            (self.disburs['payment_made'] <= max_disburs_date)
+            & (self.disburs['payment_made'] >= min_disburs_date)
+        ]
+
+    def _get_employees(self):
+        return pd.concat([
+            pd.Series(df['employee_code'].unique()) for df in (self.payments, self.disburs)]).unique()
+
 
 class EmpSuperData(SuperData):
     """Contains payment and super disbursement data frames for a single emp"""
@@ -25,9 +46,13 @@ class EmpSuperData(SuperData):
     def __init__(self, super_data: SuperData, employee_code: int):
         self.employee_code = employee_code
 
-        payments = super_data.payments
-        disburs = super_data.disburs
+        payments = super_data.payments.copy()
+        disburs = super_data.disburs.copy()
+
         super().__init__(
             payments=payments.loc[payments['employee_code'] == employee_code],
             disburs=disburs.loc[disburs['employee_code'] == employee_code]
         )
+
+    def __str__(self):
+        return f"EmpSuperData:{self.employee_code}:({len(self.payments)} payments; {len(self.disburs)} disbursments)"
