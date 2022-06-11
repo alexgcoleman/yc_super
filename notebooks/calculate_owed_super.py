4d01023e62f8818f20554152d8e9a5ee54266f95
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2022-06-11 17:14:02 +1000

    Calculate owed super per period Fixes #1

--- notebooks/calculate_owed_super.py
+++ notebooks/calculate_owed_super.py
@@ -1,17 +1,23 @@
 # %% [markdown]
 # # Calculating Owed Superannuation
 # %%
-from yc_super.superdata import SuperData
-from yc_super.readers import read_combined_file
+from __future__ import annotations
 
+from yc_super.readers import read_combined_file
+from yc_super.owed import calculate_owed
 import pandas as pd
 
 from pathlib import Path
 
 super_file = Path('data', 'Sample Super Data.xlsx')
 
-# %%
-super_data = read_combined_file(super_file)
+SUPER_PERC = 0.095
 
 # %%
-super_data.payments
+super_data = read_combined_file(super_file)
+payments = super_data.payments
+# %% [markdown]
+# ## Categorising payments into quarters
+# %%
+owed = calculate_owed(super_data)
+owed
