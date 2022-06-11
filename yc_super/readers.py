e8802dc49c84afd2ef52c248966f2a90b9b0cd74
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2022-06-12 00:38:15 +1000

    Combine overlapping disbursements Fixes #10
    Also add corresponding payslip data

--- yc_super/readers.py
+++ yc_super/readers.py
@@ -61,7 +61,9 @@ def parse_disburs(raw_disburs: pd.DataFrame) -> pd.DataFrame:
             closed='both'), axis=1)
 
     disburs = disburs.drop(
-        columns=['pay_period_from', 'pay_period_to'])
+        columns=['pay_period_from', 'pay_period_to']).rename(columns={
+            'sgc_amount': 'amount'
+        })
 
     return disburs
 
@@ -104,26 +106,113 @@ def get_disbur_idx_for_payslip(payslip_row: pd.Series, disburs: pd.DataFrame) ->
     )
 
 
-def filter_future_disburs(disburs_df: pd.DataFrame, payments: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """Filters disbursements that cover a period after the latest payment)
+@pd.api.extensions.register_series_accessor("interval")
+class IntervalAccessor:
+    """Implementing custom accessor as using a lot of interval methods"""
 
-    Returns Tuple (past, future), representing past and future (excluded) disbursements"""
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
+
+    @property
+    def left(self):
+        return self._obj.apply(lambda x: x.left)
+
+    @property
+    def right(self):
+        return self._obj.apply(lambda x: x.right)
+
+    @property
+    def length(self):
+        return self._obj.apply(lambda x: x.length)
+
+
+def can_absorb(interval_1, interval_2: pd.Series) -> bool:
+    """Returns True if disbur_1 can absorb disbur_2"""
+    return ((interval_1.left <= interval_2.left)
+            and (interval_1.right >= interval_2.right))
+
+
+def absorb_disburs_for_emp(emp_disburs_df: pd.DataFrame) -> pd.DataFrame:
+    """"""
+    emp_disburs = emp_disburs_df
+    emp_disburs['_period_length'] = emp_disburs['pay_period'].interval.length
+
+    # sort descending payment_made date and period_length
+    # now we only need to consider if a row absorbs the next row
+    emp_disburs = emp_disburs.sort_values(
+        ['payment_made', '_period_length'], ascending=False)
+
+    # finding what to absorb
+    # TODO: below line seems to be a bug in this pandas? unable to shift
+    # series that contains intervals, seems to try and concat?
+    # bypassing by reconstructin interval from next row
+    #emp_disburs['_next_period'] = emp_disburs['pay_period'].shift(-1)
+    emp_disburs['_next_left'] = emp_disburs['pay_period'].interval.left.shift(
+        -1).fillna(emp_disburs['pay_period'].interval.left) + pd.DateOffset(1)
+    emp_disburs['_next_right'] = emp_disburs['pay_period'].interval.right.shift(
+        -1).fillna(emp_disburs['pay_period'].interval.right)
+    emp_disburs['_next_period'] = emp_disburs.apply(
+        lambda row: pd.Interval(row['_next_left'], row['_next_right'], closed='both'), axis=1)
+
+    emp_disburs['_next_amount'] = emp_disburs['amount'].shift(-1, fill_value=0)
+
+    emp_disburs['_absorb_next'] = emp_disburs.apply(
+        lambda row: can_absorb(row['pay_period'], row['_next_period']), axis=1)
+
+    emp_disburs['_was_absorbed'] = emp_disburs['_absorb_next'].shift(
+        1, fill_value=False)
+
+    # absorbing
+    # TODO: below technique doesn't work if there are multiple concurrent absorbtions! just raise error for now
+    emp_disburs['_absorb_next_next'] = emp_disburs['_absorb_next'].shift(1)
+    if sum(emp_disburs['_absorb_next'] & emp_disburs['_absorb_next_next']) > 0:
+        raise ValueError('detected multiple concurrent absorptions!')
+
+    # adding totals from absorbees
+    rows_arbsorbers = emp_disburs['_absorb_next']
+    emp_disburs.loc[rows_arbsorbers,
+                    'amount'] += emp_disburs.loc[rows_arbsorbers, '_next_amount']
+
+    # returnign unabsorbed rows, with original columns
+    return emp_disburs.loc[~emp_disburs['_was_absorbed'], list(emp_disburs_df)]
+
+
+def absorb_disburs(disburs_df: pd.DataFrame) -> pd.DataFrame:
+    """Makes disbursements with a pay_period that entirely encompass 
+    other disbursements' pay_periods "absorb" the smaller disbursements,
+    adding their total to its own.
+
+    Returns disbursements DataFrame
+    """
+    return pd.concat([
+        disburs_df.loc[disburs_df['employee_code'] == emp]
+        .copy()
+        .pipe(absorb_disburs_for_emp)
+        for emp in disburs_df['employee_code'].unique()
+    ])
+
+
+def combine_disburse_payments(disburs_df: pd.DataFrame, payments_df: pd.DataFrame) -> pd.DataFrame:
+    """Returns disburments, with matching payments data added as a nested series"""
     disburs = disburs_df.copy()
+    payments = payments_df.copy()
 
-    emp_payment_ranges = payments.groupby('employee_code').agg(
-        _payment_max_date=('end', 'max'))
+    payments['_disbur_idx'] = payments.apply(
+        get_disbur_idx_for_payslip, axis=1, disburs=disburs).astype("Int64")
 
-    disburs = disburs.merge(
-        emp_payment_ranges, on='employee_code', how='left')
-
-    rows_future_disburs = disburs['pay_period'].apply(
-        lambda x: x.right) > disburs['_payment_max_date']
-
-    # can return input df, as indexes will be aligned - ensures we don't mutate it accidentally
-    return (
-        disburs_df[~rows_future_disburs],
-        disburs_df[rows_future_disburs]
+    # getting payment data for disbursments
+    disburs_payment_data = (
+        pd.DataFrame({'_disbur_idx': disburs.index})
+        .merge(payments.fillna(-1), on='_disbur_idx', how='left')
+        .groupby('_disbur_idx')
+        .agg(
+            payslip_ids=('payslip_id', 'unique'),
+            payslip_quarters=('quarter', 'unique'),
+            payslip_num_quarters=('quarter', 'nunique')
+        )
     )
+
+    return disburs.join(disburs_payment_data)
 
 
 def read_combined_file(path: Path) -> SuperData:
@@ -135,15 +224,12 @@ def read_combined_file(path: Path) -> SuperData:
         raw_pay_slips=excel.parse('Payslips')
     )
 
-    # TODO: do something with future_disburs! log, save in SuperData for rainy day? could just flag?
-    disburs, future_disburs = (
+    disburs = (
         excel
         .parse('Disbursements')
         .pipe(parse_disburs)
-        .pipe(filter_future_disburs, payments))
-
-    # adding disbursement_id to payments
-    payments['_disbur_idx'] = payments.apply(
-        get_disbur_idx_for_payslip, axis=1, disburs=disburs)
+        .pipe(absorb_disburs)
+        .pipe(combine_disburse_payments, payments_df=payments)
+    )
 
     return SuperData(payments=payments, disburs=disburs)
