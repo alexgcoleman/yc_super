be866fd7b29f09930b81f6ff5a065fd741e109eb
alexgcoleman <107187057+alexgcoleman@users.noreply.github.com>
2022-06-12 13:23:48 +1000

    Implement SuperData.audit_super_emp_quarter
    Closes #14
    Closes #12
    Closes #3
    Closes #2

--- notebooks/calc_paid_super.py
+++ notebooks/calc_paid_super.py
@@ -11,14 +11,10 @@ from pathlib import Path
 
 DATA_PATH = Path('data', 'Sample Super Data.xlsx')
 EXAMPLE_PATH = Path('data', 'example.xlsx')
-
-super_file = EXAMPLE_PATH
-
 SUPER_PERC = 0.095
 
-super_data = read_combined_file(super_file)
+super_data = read_combined_file(EXAMPLE_PATH)
+audit = super_data.audit_super_emp_quarter()
 
-# %%
-withheld = super_data.withheld_super_per_emp_quarter()
-withheld
+audit
 # %%
--- yc_super/superdata.py
+++ yc_super/superdata.py
@@ -3,15 +3,25 @@ from yc_super.yearquarter import YearQuarter
 import pandas as pd
 
 
+SUPER_PERC = 0.095
+
+
 class SuperData:
     """Contains payment and super disbursment data frames - contains data for all emps"""
 
-    def __init__(self, payments: pd.DataFrame, disburs: pd.DataFrame):
+    payments: pd.DataFrame
+    disburs: pd.DataFrame
+    employees: pd.Series
+    quarters: pd.Series
+    super_perc: float
+
+    def __init__(self, payments: pd.DataFrame, disburs: pd.DataFrame, super_perc: float = SUPER_PERC):
         self.payments = payments
         self.disburs = disburs
 
         self.employees = self._get_unique_values('employee_code')
         self.quarters = self._get_unique_values('quarter')
+        self.super_perc = super_perc
 
     def __str__(self):
         return f"SuperData({len(self.payments)} payments; {len(self.disburs)} disbursments)"
@@ -32,7 +42,7 @@ class SuperData:
             for df in dfs
         ])
 
-    def withheld_super_per_emp_quarter(self) -> pd.DataFrame:
+    def withheld_per_emp_quarter(self) -> pd.DataFrame:
         """Returns a multi-indexed dataframe containing the amount
         of withheld super for each employee for each quarter"""
         return (
@@ -41,3 +51,52 @@ class SuperData:
             .groupby(['employee_code', 'quarter'])
             .agg(withheld=('amount', 'sum'))
         )
+
+    def disbursed_per_emp_quarter(self) -> pd.DataFrame:
+        """Amount of disbursed super for each employee for each quarter"""
+        return (
+            self.disburs
+            .groupby(['employee_code', 'quarter'])
+            .agg(disbursed=('amount', 'sum'))
+        )
+
+    def ote_and_entitled_super_per_emp_quarter(self) -> pd.DataFrame:
+        """Sum of the OTE earned per employee per quarter"""
+        ote = (
+            self.payments
+            .loc[self.payments['is_ote']]
+            .groupby(['employee_code', 'quarter'])
+            .agg(ote=('amount', 'sum'))
+        )
+
+        ote['entitled'] = ote['ote'] * self.super_perc
+
+        return ote
+
+    def audit_super_emp_quarter(self) -> pd.DataFrame:
+        """
+        Audits the super values for an employee for a quarter.
+
+        MultiIndexed on employee_code, quarter
+
+        Fields:
+          - ote : total Ordinary time earnings
+          - entitled: total entitled super (ote * super_perc)
+          - withheld: total withheld super
+          - disbursed: total disbursed super
+
+        Additional Delta fields for auditing:
+          - entitled-withheld: +'ve = entitled more than withheld
+          - entitled-disbursed : +'ve = entitled more than disbursed
+          - withheld-disbursed: +'ve = withheld more than disbursed 
+        """
+        audit = (self.ote_and_entitled_super_per_emp_quarter()
+                 .join(self.disbursed_per_emp_quarter(), how='outer')
+                 .join(self.withheld_per_emp_quarter(), how='outer')
+                 .fillna(0)
+                 )
+
+        audit['entitled-withheld'] = audit['entitled'] - audit['withheld']
+        audit['entitled-disbursed'] = audit['entitled'] - audit['disbursed']
+        audit['withheld-disbursed'] = audit['withheld'] - audit['disbursed']
+        return audit
